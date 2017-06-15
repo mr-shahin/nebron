b91c7711fcbc325332ebc1ea7927dfd86fd085ba
mr-shahin <mryavar001100@gmail.com>
2017-06-15 13:24:15 +0430

    Add files via upload

---
+++ plugins/servers.lua
@@ -0,0 +1,15 @@
+local function run(msg, matches)
+local text = io.popen("sh ./data/cmd.sh"):read('*all')
+if is_sudo(msg) then
+  return text
+end
+  end
+return {
+  patterns = {
+    '^مشخصات سرور$',
+    '^serverinfo$',
+  },
+  run = run,
+  moderated = true
+}
+
